2e484de7b41f6b03b998b2c16a12572798f21812
Aravindh <aravindhbme@gmail.com>
2021-04-23 15:07:39 +0530

    first commit

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("enter string");
+            string inp = Console.ReadLine();
+            char[] rev = new char[inp.Length];
+            for(int i=inp.Length-1;i>=0;i--)
+            {
+                rev[i] = inp[i];
+            }
+            string rev1 = new string(rev);
+            if (rev1==inp)
+            
+            
+            { Console.WriteLine("palindrome"); }
+            else
+            { Console.WriteLine("Not palindrome"); }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            string str=Console.ReadLine();
+            int i,  Vowel_count;
+            Console.WriteLine("Enter string");
+            Vowel_count = 0;
+            
+            for (i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'||str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U' )
+            {
+                Vowel_count++;
+            }
+            Console.WriteLine("vowel count", Vowel_count);
+        }
+            Console.ReadLine();
+
+        }
+
+            
+                
+
+        }
+    }
+
