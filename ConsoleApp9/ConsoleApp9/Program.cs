76be93e50459765301adb4eff17b895ef8742cbd
Aravindh <aravindhbme@gmail.com>
2021-04-23 12:57:04 +0530

    first commit

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class emp
+    {
+        public int empID { get; set; }
+        public string name { get; set; }
+    }
+    class class8
+    {
+        static void Main(string[] args)
+        {
+            emp emp1 = new emp();
+            emp1.empID = 101;
+            emp1.name = "Dev";
+            emp emp2 = new emp();
+            emp2.empID = 102;
+            emp2.name = "Abd";
+            Console.WriteLine(emp1.Equals(emp2));
+            Console.WriteLine(Equals(emp1,emp2));
+            Console.ReadLine();
+
+
+        }
+    }
+}
