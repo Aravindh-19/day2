56a9bfa048fc2e04b1353db1164e69406254f792
Aravindh <aravindhbme@gmail.com>
2021-04-23 15:30:51 +0530

    first commit

--- ConsoleApp13/ConsoleApp13/Program.cs
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -8,8 +8,44 @@ namespace ConsoleApp13
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            string str;
+            int[] ch_fre = new int[255];
+            int i = 0, max, l;
+            int ascii;
+
+            Console.Write("\n\nFind maximum occurring character in a string :\n");
+            Console.Write("--------------------------------------------------\n");
+            Console.Write("Input the string : ");
+            str = Console.ReadLine();
+            l = str.Length;
+
+            for (i = 0; i < 255; i++)  //Set frequency of all characters to 0
+            {
+                ch_fre[i] = 0;
+            }
+            
+            i = 0;
+            while (i < l)
+            {
+                ascii = (int)str[i];
+                ch_fre[ascii] += 1;
+
+                i++;
+            }
+            
+            max = 0;
+            for (i = 0; i < 255; i++)
+            {
+                if (i != 32)
+                {
+                    if (ch_fre[i] > ch_fre[max])
+                        max = i;
+                }
+            }
+            Console.Write("The most used  character '{0}' is appearing for number of times : {1} \n\n", (char)max, ch_fre[max]);
+            Console.ReadLine();
         }
     }
 }
