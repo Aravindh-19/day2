ea6340b1840802fd2e0df37b744b233be4cc99c7
Aravindh <aravindhbme@gmail.com>
2021-04-23 10:45:28 +0530

    first commit

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class class1
+    {
+        void Swap(ref int a,ref int b)
+        {
+
+            
+            
+
+            Console.WriteLine(+a);
+            Console.WriteLine(+b);
+            
+            Console.ReadLine();
+
+        }
+
+        static void Main()
+
+        {
+            class1 class1 = new class1();
+            int a = 5;
+             int   b = 6;
+            class1.Swap  (ref a , ref b);
+            Console.ReadLine();
+        }
+    }
+}
+
