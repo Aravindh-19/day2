87b6f3757cecf043e0eb594534722beb23dd4806
Aravindh <aravindhbme@gmail.com>
2021-04-23 15:52:43 +0530

    first commit

---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        public static void addstring(string s1, string s2,string s3)
+        {
+            Console.WriteLine(s1 + s2+s3);
+        }
+        static void Main(string[] args)
+        {
+            addstring(s1: "Dev", s2: "Virat", s3: "Abd");
+            Console.ReadLine();
+        }
+    }
+}
