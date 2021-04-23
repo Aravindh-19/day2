9fbffedc9249548f5a3cb9d42b0d622d54519f2e
Aravindh <aravindhbme@gmail.com>
2021-04-23 10:05:21 +0530

    first commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+    class class1
     {
-        static void Main(string[] args)
+        void checkoddeven(int a)
         {
+
+            if (a % 2 == 0)
+
+            { Console.WriteLine("Even"); }
+            else
+            { Console.WriteLine("odd"); }
+        }
+        
+         static void Main(string[] args)
+
+        {
+            class1 class1= new class1();
+            class1.checkoddeven(6);
+        Console.ReadLine();
         }
     }
 }
