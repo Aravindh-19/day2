aad351529064f915436dd5a981faf302a0439d67
Aravindh <aravindhbme@gmail.com>
2021-04-23 11:52:01 +0530

    first commit

--- ConsoleApp6/ConsoleApp6/Program.cs
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp6
 {
-    class Program
+    class Author
     {
+        public string Name1;
+        public int rank1;
+    }
+    class class1
+    {
+        public string Name2;
+        public int rank2;
         static void Main(string[] args)
         {
+            class1 class1 = new class1();
+            Author author = new Author();
+            bool result;
+            result = class1 is Author;
+            Console.WriteLine(result);
+            Console.ReadLine();
+
         }
     }
 }
