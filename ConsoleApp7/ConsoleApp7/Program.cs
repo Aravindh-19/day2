102dc2b99957f77d50be42bfc0654c3a1354837c
Aravindh <aravindhbme@gmail.com>
2021-04-23 12:44:45 +0530

    first commit

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+
+{
+    public struct person
+    {
+        public string name { get; set; }
+        public person(string _name)
+        {
+            name = _name;
+        }
+    }
+    public struct dog
+    {
+        public String Breed { get; set; }
+        public dog(string _breed)
+        {
+            Breed = _breed;
+        }
+    }
+    class Program
+    {
+        public static void showvalue(object o)
+
+        {
+            if(o is person p )
+            {
+                Console.WriteLine(p.name);
+            }
+            else if(o is dog d)
+            {
+                Console.WriteLine(d.Breed);
+            }
+
+        }
+        static void Main()
+        {
+            object o = new person("Abd");
+            showvalue(o);
+            o = new dog("Tom");
+            showvalue(o);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Dog
+    {
+        public void speak()
+        {
+            Console.WriteLine("Bark !");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object o = new Dog();
+            Dog dog = o as Dog;
+            if (dog != null)
+                dog.speak();
+            Console.ReadLine();
+        }
+    }
+}
